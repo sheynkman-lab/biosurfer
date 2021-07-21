475c71dca0de587cd0f0fea49a872d2fe2b072a3
Jared Saquing <xht7mt@virginia.edu>
2021-07-21 19:46:04 -0400

    move gene dictionary creation/loading into own function

--- test_isocreatealign.py
+++ test_isocreatealign.py
@@ -52,36 +52,40 @@ genes = (
     'SELENOW',
     'TIMM50',
 )
-temp_file_path = data_dir + f'gene_dict_{"_".join(sorted(genes))}.p'
 
-try:
-    with open(temp_file_path, 'rb') as f:
-        gd = pickle.load(f)
-        print("loaded dict from pickle file")
-except IOError:
-    print("loading dict from gtf and fasta files")
-    # load data
-    orf_seqs = helpers.gc_fasta_to_orf_seq_dict(path_transcripts_fa)
-    chr_dict = helpers.load_hg38(path_chr_fa)
-    # hg38_dict = helpers.load_hg38(path_hg38_fa)
-    # domains = helpers.load_domain_mappings(path_pfam_gc, path_pfam_names)
-    # aln_data = helpers.load_clustal_alignments(path_gc_aln)
-    # aln_blocks = helpers.load_alignment_block_calls(aln_data, path_gc_aln_blocks)
-    # tf_sachi, tf_gloria, tf_lambert = helpers.load_tf_list(path_tf_list_sachi,
-    #                                      path_tf_list_gloria, path_tf_list_lambert)
-    # tfs = helpers.load_man_correct_gc30_tf_genenames(path_gc_corr_tf_list)
-    # appris_orfs = helpers.load_appris_principle_isonames(path_appris)
-    # isoacc_map = helpers.load_6k_isoacc_map(path_isoacc)
+def get_gene_dictionary(gene_list):
+    temp_file_path = data_dir + f'gene_dict_{"_".join(sorted(gene_list))}.p'
+    try:
+        with open(temp_file_path, 'rb') as f:
+            gd = pickle.load(f)
+            print("loaded dict from pickle file")
+    except IOError:
+        print("loading dict from gtf and fasta files")
+        # load data
+        orf_seqs = helpers.gc_fasta_to_orf_seq_dict(path_transcripts_fa)
+        chr_dict = helpers.load_hg38(path_chr_fa)
+        # hg38_dict = helpers.load_hg38(path_hg38_fa)
+        # domains = helpers.load_domain_mappings(path_pfam_gc, path_pfam_names)
+        # aln_data = helpers.load_clustal_alignments(path_gc_aln)
+        # aln_blocks = helpers.load_alignment_block_calls(aln_data, path_gc_aln_blocks)
+        # tf_sachi, tf_gloria, tf_lambert = helpers.load_tf_list(path_tf_list_sachi,
+        #                                      path_tf_list_gloria, path_tf_list_lambert)
+        # tfs = helpers.load_man_correct_gc30_tf_genenames(path_gc_corr_tf_list)
+        # appris_orfs = helpers.load_appris_principle_isonames(path_appris)
+        # isoacc_map = helpers.load_6k_isoacc_map(path_isoacc)
 
-    gd = isocreate.init_gen_obj_gc(path_chr_gtf, gene_list=genes)
-    gd = isocreate.create_and_link_seq_related_obj(gd, orf_seqs)
-    gd = isocreate.create_and_link_junct_and_ss_objs(gd, chr_dict)
-    # gd = isocreate.create_and_map_domains(gd, domains)
+        gd = isocreate.init_gen_obj_gc(path_chr_gtf, gene_list=gene_list)
+        gd = isocreate.create_and_link_seq_related_obj(gd, orf_seqs)
+        gd = isocreate.create_and_link_junct_and_ss_objs(gd, chr_dict)
+        # gd = isocreate.create_and_map_domains(gd, domains)
 
-    with open(temp_file_path, 'wb') as f:
-        pickle.dump(gd, f)
+        with open(temp_file_path, 'wb') as f:
+            pickle.dump(gd, f)
 
-assert set(genes) <= gd.keys()
+    assert set(gene_list) <= gd.keys()
+    return gd
+
+gd = get_gene_dictionary(genes)
 aln_grp_dict = dict()
 
 # %%
