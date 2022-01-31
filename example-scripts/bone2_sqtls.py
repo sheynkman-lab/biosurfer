355bd07d2f36d99328e3c3c54e6824bb2bafc2e7
Jamie Saquing <xht7mt@virginia.edu>
2022-01-31 14:55:03 -0500

    update data files for bone2 sQTL analysis and add 1 CPM filter to script

--- example-scripts/bone2_sqtls.py
+++ example-scripts/bone2_sqtls.py
@@ -3,7 +3,7 @@ import csv
 import multiprocessing as mp
 import os
 import sys
-from itertools import groupby, islice, product
+from itertools import groupby, product
 from operator import attrgetter
 from statistics import median
 
@@ -26,7 +26,7 @@ from IPython.display import display
 from matplotlib.gridspec import GridSpec
 from matplotlib.colors import LogNorm
 from scipy.sparse import coo_matrix
-from sqlalchemy.sql.expression import and_, or_, not_
+from sqlalchemy import and_, or_, not_, select
 from tqdm import tqdm
 
 plt.switch_backend('agg')
@@ -64,15 +64,14 @@ expression['total'] = expression.sum(axis=1)
 expression['frac_total'] = expression['total'] / sum(expression['total'])
 for days in (0, 2, 4, 10):
     expression[f't{days}'] = expression[[col for col in expression.columns if col.startswith(f't{days}')]].mean(axis=1)
+expression['abundant'] = expression[['t0', 't2', 't4', 't10']].applymap(lambda x: x >= 1.0).any(axis=1)
 
 # %%
 print('Loading sQTL table...')
 sqtls_raw = pd.read_csv(f'{data_dir}/coloc_sig_pc_full.tsv', sep='\t', nrows=None)
 sqtls = sqtls_raw[sqtls_raw['gene_type'] == 'protein_coding']
 # sqtls = sqtls.sample(frac=0.01, axis=0)
-# with open(f'{data_dir}/problems.txt') as f:
-#     problems = set(line.strip() for line in f)
-# sqtls = sqtls[sqtls['gene_name'].isin(problems)]
+# sqtls = sqtls[sqtls['gene_name'].isin(['TPM2', 'PGS1', 'ITGA1', 'TMEM263'])]
 def optional_list(things):
     list_things = sorted(set(things))
     if len(list_things) == 1:
@@ -84,35 +83,47 @@ def stem(accession):
     return accession.split('.')[0]
 def similar(acc1, acc2):
     return stem(acc1) == stem(acc2)
-gene_id_stems = {stem(gene_id) for gene_id in sqtls['gene']}
-gene_ids = {row.accession for row in
-    session.query(Gene.accession).where(
-        or_(
-            False,
-            *(Gene.accession.startswith(gene_id_stem) 
-                for gene_id_stem in gene_id_stems)
+
+gene_id_to_name = {
+    row.accession: row.name
+    for row in session.query(Gene.accession, Gene.name).where(
+        and_(
+            not_(Gene.accession.contains('_', autoescape=True)),
+            or_(
+                False,
+                *(Gene.accession.startswith(stem(gene_id)) 
+                    for gene_id in sqtls['gene'].unique())
+            )
         )
     )
 }
-# db.project_feature_mappings(gene_ids=gene_ids)
+# db.project_feature_mappings(gene_ids=gene_id_to_name.keys())
+gene_id_stem_to_name = {stem(k): v for k, v in gene_id_to_name.items()}
+minus_strand_genes = {stem(row.accession) for row in session.query(Gene.accession).where(Gene.strand == Strand.MINUS)}
 
 # %%
-gene_query = session.query(Gene).join(Gene.transcripts).\
+q = (
+    select(Transcript).
     where(
         and_(
             Transcript.sequence != None,
-            Gene.accession.in_(gene_ids),
-            not_(Gene.accession.contains('_', autoescape=True))
+            Transcript.gene_id.in_(gene_id_to_name.keys()),
+            or_(
+                Transcript.type == 'gencodetranscript',
+                Transcript.accession.in_(expression[expression['abundant']].index)
+            )
         )
-    )
+    ).
+    order_by(Transcript.gene_id)
+)
 # print(str(gene_query))
 print('Loading database objects...')
-genes = {stem(gene.accession): gene for gene in gene_query}
+gene_id_stem_to_transcripts = {
+    stem(gene_id): list(transcripts)
+    for gene_id, transcripts in groupby(session.execute(q).scalars(), key=attrgetter('gene_id'))
+}
 
 # %%
-# def abundant_and_coding(transcript: 'Transcript'):
-#     return transcript.accession in over3counts and len(transcript.orfs) > 0
-
 def sortkey(transcript: 'Transcript'):
     return -expression.total[transcript.accession], getattr(transcript, 'sqanti', SQANTI.OTHER)
 
@@ -125,29 +136,35 @@ def abundance_str(transcript: 'Transcript'):
 def get_augmented_sqtl_record(row):
     chr, gene_id, start, end, h4 = row
     try:
-        gene = genes[stem(gene_id)]
+        transcripts = gene_id_stem_to_transcripts[stem(gene_id)]
     except KeyError:
         return None
-    if gene.strand is Strand.MINUS:
+    gene_name = gene_id_stem_to_name[stem(gene_id)]
+    if stem(gene_id) in minus_strand_genes:
+        strand = Strand.MINUS
         start, end = end, start
-    junc = Junction(start, end, chr, gene.strand)
+    else:
+        strand = Strand.PLUS
+    junc = Junction(start, end, chr, strand)
     junc_info = {
         'chr': chr,
         'strand': str(junc.strand),
         'donor': start,
         'acceptor': end,
-        'gene': gene.name,
+        'gene': gene_name,
         'H4': h4,
         'pairs': 0
     }
-    pb_transcripts = {tx for tx in gene.transcripts if isinstance(tx, PacBioTranscript) and all(orf.has_stop_codon for orf in tx.orfs)}
+    gene_start = min(tx.start for tx in transcripts)
+    gene_stop = max(tx.stop for tx in transcripts)
+    pb_transcripts = {tx for tx in transcripts if isinstance(tx, PacBioTranscript)}
     not_using, using = split_transcripts_on_junction_usage(junc, filter(attrgetter('orfs'), pb_transcripts))
     using = sorted(using, key=sortkey)
     not_using = sorted(not_using, key=sortkey)
     if not using or not not_using:
         return None
     
-    gc_transcripts = sorted((tx for tx in gene.transcripts if isinstance(tx, GencodeTranscript)), key=attrgetter('appris'), reverse=True)
+    gc_transcripts = sorted((tx for tx in transcripts if isinstance(tx, GencodeTranscript)), key=attrgetter('appris'), reverse=True)
     anchor_tx = gc_transcripts[0]
 
     pairs = list(product(not_using, using))
@@ -162,7 +179,7 @@ def get_augmented_sqtl_record(row):
 
     # calculate fraction of pairs where one isoform is NMD and other is not
     junc_info['NMD'] = None
-    with ExceptionLogger(f'Error for {gene.name} {junc}'):
+    with ExceptionLogger(f'Error for {gene_name} {junc}'):
         junc_info['NMD'] = sum(tx1.primary_orf.nmd ^ tx2.primary_orf.nmd for tx1, tx2 in pairs) / n_pairs
 
     # calculate median change in sequence length for junction-related pblocks
@@ -177,13 +194,13 @@ def get_augmented_sqtl_record(row):
     freqs = {event.name: count/n_pairs for event, count in counts.items()}
     junc_info.update(freqs)
 
-    if not os.path.isdir(f'{output_dir}/{gene.name}'):
-        os.mkdir(f'{output_dir}/{gene.name}')
+    if not os.path.isdir(f'{output_dir}/{gene_name}'):
+        os.mkdir(f'{output_dir}/{gene_name}')
 
-    export_annotated_pblocks_to_tsv(f'{output_dir}/{gene.name}/{gene.name}_{junc.donor}_{junc.acceptor}.tsv', pblocks)
+    export_annotated_pblocks_to_tsv(f'{output_dir}/{gene_name}/{gene_name}_{junc.donor}_{junc.acceptor}.tsv', pblocks)
 
     force_plotting = False
-    fig_path = f'{output_dir}/{gene.name}/{gene.name}_{junc.donor}_{junc.acceptor}.png'
+    fig_path = f'{output_dir}/{gene_name}/{gene_name}_{junc.donor}_{junc.acceptor}.png'
     if force_plotting or not os.path.isfile(fig_path):
         isoplot = IsoformPlot(
             gc_transcripts + [None] + using + not_using + [None],
@@ -193,11 +210,11 @@ def get_augmented_sqtl_record(row):
                 # 'CPM': abundance_str,
             }
         )
-        with ExceptionLogger(f'Error plotting {gene.name} {junc}'):
+        with ExceptionLogger(f'Error plotting {gene_name} {junc}'):
             gs = GridSpec(1, 5)
             isoplot.draw_all_isoforms(subplot_spec=gs[:-1])
             isoplot.draw_frameshifts(anchor=anchor_tx)
-            isoplot.draw_region(type='line', color='k', linestyle='--', linewidth=1, track=len(gc_transcripts) + len(using) + 0.5, start=gene.start, stop=gene.stop)
+            isoplot.draw_region(type='line', color='k', linestyle='--', linewidth=1, track=len(gc_transcripts) + len(using) + 0.5, start=gene_start, stop=gene_stop)
             isoplot.draw_background_rect(start=junc.donor, stop=junc.acceptor, facecolor='#f0f0f0')
             for pblock in pblocks:
                 isoplot.draw_protein_block(pblock, alpha=n_pairs**-0.5)
@@ -206,8 +223,9 @@ def get_augmented_sqtl_record(row):
             heatmap_ax = isoplot.fig.add_subplot(gs[-1])
             all_accessions = [getattr(tx, 'accession', None) for tx in isoplot.transcripts]
             pb_accessions = [getattr(tx, 'accession', None) for tx in isoplot.transcripts if isinstance(tx, PacBioTranscript)]
-            expression_sub = expression.loc[pb_accessions].iloc[:, -4:]
+            expression_sub = expression.loc[pb_accessions][['t0', 't2', 't4', 't10']]
             expression_sub = expression_sub.reindex(index=all_accessions, fill_value=0)
+            # expression_sub['total'] = expression_sub.sum(axis=1)
             expression_sub.iloc[-1, :] = expression_sub.sum(axis=0)
             heatmap = sns.heatmap(
                 expression_sub,
@@ -221,10 +239,10 @@ def get_augmented_sqtl_record(row):
                 norm = LogNorm(vmin=0.1, vmax=10000.0),
                 mask = expression_sub.applymap(lambda x: x == 0),
                 annot = expression_sub,
-                fmt = '0.1f'
+                fmt = '0.0f'
             )
             heatmap.set_ylabel(None)
-            isoplot.fig.set_size_inches(16, 0.35*len(gene.transcripts))
+            isoplot.fig.set_size_inches(12, 0.35*len(transcripts))
             plt.savefig(fig_path, facecolor='w', transparent=False, dpi=200, bbox_inches='tight')
             tqdm.write('\tsaved '+fig_path)
         plt.close(isoplot.fig)
--- example-scripts/load_bone2_database.py
+++ example-scripts/load_bone2_database.py
@@ -10,9 +10,9 @@ gencode_tl = 'gencode/gencode.v38.pc_translations.fa'
 gencode_doms = 'gencode/grch38-protein-features.tsv'
 pfam_dom_info = 'gencode/pfamA.tsv'
 prosite_pattern_info = 'gencode/prosite.dat'
-pacbio_gtf = 'bone2/hfob_CDS_with_transcripts_with_cds.gtf' 
+pacbio_gtf = 'bone2/hfob_CDS_with_transcripts_min30orf_with_cds.gtf' 
 pacbio_tx = 'bone2/SQANTI3_results_full_corrected.fasta'
-pacbio_tl = 'bone2/hfobs_orf_refined.fasta'
+pacbio_tl = 'bone2/hfobs_att2_orf_refined.fasta'
 sqanti = 'bone2/SQANTI3_results_full_classification.txt'
 
 def get_ids_from_pacbio_fasta(header: str):
@@ -27,13 +27,13 @@ def skip_gencode(header: str):
 
 #%%
 db = Database('bone2')
-# db.recreate_tables()
+db.recreate_tables()
 start = time()
-# db.load_gencode_gtf(path + gencode_gtf)
-# db.load_transcript_fasta(path + gencode_tx, get_ids_from_gencode_fasta, skip_par_y)
-# db.load_translation_fasta(path + gencode_tl, get_ids_from_gencode_fasta, skip_par_y)
-# db.load_pacbio_gtf(path + pacbio_gtf)
-# db.load_transcript_fasta(path + pacbio_tx, get_ids_from_pacbio_fasta)
+db.load_gencode_gtf(path + gencode_gtf)
+db.load_transcript_fasta(path + gencode_tx, get_ids_from_gencode_fasta, skip_par_y)
+db.load_translation_fasta(path + gencode_tl, get_ids_from_gencode_fasta, skip_par_y)
+db.load_pacbio_gtf(path + pacbio_gtf)
+db.load_transcript_fasta(path + pacbio_tx, get_ids_from_pacbio_fasta)
 db.load_translation_fasta(path + pacbio_tl, get_ids_from_hybrid_fasta, skip_gencode)
 db.load_sqanti_classifications(path + sqanti)
 db.load_domains(path + pfam_dom_info)
