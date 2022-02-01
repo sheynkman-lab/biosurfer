e5ec74cd471ea4e412f4dd3b5db9f4e4d478f6f2
Jamie Saquing <xht7mt@virginia.edu>
2022-01-31 19:35:47 -0500

    revise tests for BisectDict

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,11 +1,18 @@
 from itertools import combinations, product
 from typing import Dict, List, Tuple
 
+import pytest
 from biosurfer.core.helpers import BisectDict, run_length_decode, run_length_encode
 from biosurfer.plots.plotting import generate_subtracks
 from hypothesis import given
 from hypothesis.strategies import characters, data, integers, lists, dictionaries, sampled_from, text
 from more_itertools import chunked
+
+breakpoint_dict = dictionaries(
+    integers(min_value=1),
+    characters(whitelist_categories=['Lu']),
+    min_size = 1
+)
 
 def force_list_length_to_be_even(lst: List) -> List:
     N = len(lst)
@@ -37,21 +44,27 @@ def intervals_overlap(intervals1, intervals2):
 ### TESTS BEGIN HERE ###
 
 @given(
-    dictionary = dictionaries(
-        integers(min_value=1),
-        characters(whitelist_categories=['Lu']),
-        min_size = 1
-    ),
+    dictionary = breakpoint_dict,
     data = data()
 )
 def test_bisect_dict(dictionary, data):
     bdict = BisectDict(dictionary.items())
-    breakpoints = sorted(dictionary.keys())
-    i = data.draw(integers(min_value=0, max_value=len(breakpoints)-1))
-    stop = breakpoints[i]
-    start = breakpoints[i-1] if i > 0 else 0
-    key = data.draw(integers(min_value=start, max_value=stop-1))
-    assert bdict[key] == dictionary[stop]
+    key = data.draw(integers(min_value=0, max_value=max(dictionary.keys())-1))
+    for breakpoint, v in sorted(dictionary.items()):
+        value = v
+        if key < breakpoint:
+            break
+    assert bdict[key] == value
+
+@given(
+    dictionary = breakpoint_dict,
+    data = data()
+)
+def test_bisect_dict_keyerror(dictionary, data):
+    bdict = BisectDict(dictionary.items())
+    badkey = data.draw(integers().filter(lambda x: x < 0 or x >= max(dictionary.keys())))
+    with pytest.raises(KeyError):
+        _ = bdict[badkey]
 
 @given(label_intervals_mappings.map(extract_intervals_labels))
 def test_generate_subtracks(arg):
