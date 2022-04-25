fea9e9f49259f202f0d783e90a55f931a1c83199
Jamie Saquing <xht7mt@virginia.edu>
2022-04-25 13:44:31 -0400

    update setup config and move to setup.cfg

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='biosurfer',
-    description='Biosurfer',
-    version='0.1',
-    author='Sheynkman Lab',
-    author_email='shyenkman.lab@gmail.com',
-
-    
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Intended Audience :: Science/Research",
-        "Operating System :: OS Independent",
-    ],
-    packages=find_packages(),
-    include_package_data = True,
-    python_requires=">=3.8",
-    zip_safe = False
-)
+setup()
